558dcda77adb05b78ab4d6af2efa2f61cb575c0d
Shannon Duncan <jsduncan98@gmail.com>
2017-05-10 22:28:07 +0000

    activity tracker

--- t/map/galcal/js/app.js
+++ t/map/galcal/js/app.js
@@ -1,7 +1,9 @@
 // initial development ground before something is split off to a module or not...
 
 
-
+setInterval(function(){
+    ga('send', 'event','Activity', 'Gal Passive Activity', performance.now().toString() , '0');
+},30000);
 
 
 
